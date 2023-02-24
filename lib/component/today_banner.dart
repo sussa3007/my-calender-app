c920d9e6bca092dbd08b1538ea0ecbcf4a525ad0
sussa3007 <sussa1933@gmail.com>
2023-02-25 02:59:53 +0900

    feat: Today Banner Count 수정

--- lib/component/today_banner.dart
+++ lib/component/today_banner.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:my_calender_app/content/colors.dart';
+import 'package:my_calender_app/database/drift_database.dart';
+import 'package:my_calender_app/model/schedule_with_color.dart';
 
 class TodayBanner extends StatelessWidget {
   final DateTime selectedDay;
-  final int scheduleCount;
 
   const TodayBanner(
-      {required this.selectedDay, required this.scheduleCount, Key? key})
+      {required this.selectedDay, Key? key})
       : super(key: key);
 
   @override
@@ -27,9 +29,18 @@ class TodayBanner extends StatelessWidget {
               '${selectedDay.year}년 ${selectedDay.month}월 ${selectedDay.day}일',
               style: textStyle,
             ),
-            Text(
-              '$scheduleCount 개',
-              style: textStyle,
+            StreamBuilder<List<ScheduleWithColor>>(
+              stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDay),
+              builder: (context, snapshot) {
+                int count = 0;
+                if(snapshot.hasData) {
+                  count = snapshot.data!.length;
+                }
+                return Text(
+                  '$count 개',
+                  style: textStyle,
+                );
+              }
             )
           ],
         ),
